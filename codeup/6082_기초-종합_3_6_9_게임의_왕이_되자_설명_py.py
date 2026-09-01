1791faa54a6004fc585b5b45a70c25f3bffb9ee3
Lee seohuyn <leesophy0805@gmail.com>
2026-09-01 11:31:17 +0900

    solve: codeup 6082 - [기초-종합] 3 6 9 게임의 왕이 되자(설명)(py)

---
+++ codeup/6082_기초-종합_3_6_9_게임의_왕이_되자_설명_py.py
@@ -0,0 +1,16 @@
+﻿n = int(input())
+
+for i in range(1, n+1):
+    a = i // 10
+    b = i % 10
+    
+    if a > 0:
+        if a % 3 == 0 or (b % 3 == 0 and b != 0):
+            print("X", end=" ")
+        else:
+            print(i, end=" ")
+    else :
+        if b % 3 == 0 and b != 0:
+            print("X", end=" ")
+        else:
+            print(i, end=" ")
