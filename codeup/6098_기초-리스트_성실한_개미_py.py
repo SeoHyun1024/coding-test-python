d8c5c2027f4a4d7f68f4377a01692b7b42487d6c
Lee seohuyn <leesophy0805@gmail.com>
2026-09-01 16:43:59 +0900

    solve: codeup 6098 - [기초-리스트] 성실한 개미(py)

---
+++ codeup/6098_기초-리스트_성실한_개미_py.py
@@ -0,0 +1,34 @@
+﻿matrix = []
+for i in range(11):
+    matrix.append([])
+    for j in range(11):
+        matrix[i].append(0)
+        
+for i in range(1, 11):
+    s = list(map(int, input().split()))
+    for j in range(1, 11):
+        matrix[i][j] = s[j-1]
+        
+x = 2
+y = 2
+
+while True:
+    if matrix[x][y] == 2:
+        matrix[x][y] = 9
+        break
+    
+    matrix[x][y] = 9    # 현재 위치 9로 표시
+    
+    if x >= 1 and x < 10 and y >= 1 and y < 10:
+        if  matrix[x][y+1] != 1:
+            y += 1  # 오른쪽으로 이동
+                
+        elif  matrix[x+1][y] != 1 :
+            x += 1  # 아래로 이동
+        else :
+            break
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(matrix[i][j], end=" ")
+    print()
