c4bf4640416a4ab1a7a3afda74ac1f445b0d0f4e
Lee seohuyn <leesophy0805@gmail.com>
2026-09-01 12:31:18 +0900

    solve: codeup 6092 - [기초-리스트] 이상한 출석 번호 부르기1(설명)(py)

---
+++ codeup/6092_기초-리스트_이상한_출석_번호_부르기1_설명_py.py
@@ -0,0 +1,15 @@
+﻿n = int(input())
+li = input().split()
+
+for i in range(n):
+    li[i] = int(li[i])
+    
+d = []
+for i in range(24): # 빈 리스트 생성
+    d.append(0)
+    
+for i in range(n):
+    d[li[i]] += 1
+    
+for i in range(1, 24):
+    print(d[i], end=' ')
