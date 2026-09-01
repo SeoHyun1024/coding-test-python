a9502726152ae7058ffbcfb68b5efb05ac06494d
Lee seohuyn <leesophy0805@gmail.com>
2026-09-01 15:43:51 +0900

    solve: codeup 6097 - [기초-리스트] 설탕과자 뽑기(py)

---
+++ codeup/6097_기초-리스트_설탕과자_뽑기_py.py
@@ -0,0 +1,25 @@
+﻿h, w = map(int, input().split())
+
+matrix = []
+for i in range(h+1):
+    matrix.append([])
+    for j in range(w+1):
+        matrix[i].append(0)
+        
+n = int(input())
+
+for i in range(n):
+    l, d, x, y = map(int, input().split(" "))
+    
+    for j in range(l):
+        matrix[x][y] = 1
+        
+        if d == 0:
+            y += 1
+        else :
+            x += 1
+
+for i in range(1, h+1):
+    for j in range(1, w+1):
+        print(matrix[i][j], end=" ")
+    print()
